5d857f5780778d31d703a2c5ed1406d10e8fdeef
OfirS360 <koren222ofir@gmail.com>
2025-02-24 15:32:10 +0200

    Version 1.4

--- javascript/Register.js
+++ javascript/Register.js
@@ -5,22 +5,23 @@ sumbit.onclick = () => {
         FullName: document.getElementById("full_name").value,
         Age: document.getElementById("age").value,
         ArmaExperience: document.getElementById("Yes_arma_experience").checked,
+        ArmaHours: document.getElementById("arma_hours").value,
         PreviousClans: document.getElementById("previous_clans").value,
         ClansIssues: document.getElementById("clan_issues").value,
         JoinReason: document.getElementById("join_reason").value,
         MilitaryExperience: document.getElementById("Yes_military_experience").checked,
         WeeklyHours: document.getElementById("weekly_hours").value,
-        FridayAvilable: document.getElementById("Yes_friday_availability").checked,
+        FridayAvailable: document.getElementById("Yes_friday_availability").checked,
         Avilability: document.getElementById("Yes_availability").checked
     };
-
-    console.log(getSelectedJoinValue());
 
     if (formData.FullName == "" || formData.Age == "" || formData.JoinReason == "" || formData.WeeklyHours == "" || !getSelectedJoinValue())
     {
         alert("נא למלא את החסר בטופס.")
         return;
     }
+
+    console.log(formData.FridayAvilable)
 
     fetch("http://localhost:3000/RegisterFormSend", {
         method: "POST",
@@ -50,8 +51,8 @@ function getSelectedJoinValue() {
     let No_MilitaryExperience = document.getElementById("No_military_experience");
     let Yes_FridayAvilable = document.getElementById("Yes_friday_availability"); 
     let No_FridayAvilable = document.getElementById("No_friday_availability"); 
-    let Yes_Avilable = document.getElementById("Yes_availability"); 
-    let No_Avilable = document.getElementById("No_availability"); 
+    let Yes_Avilable = document.getElementById("Yes_availability");
+    let No_Avilable = document.getElementById("No_availability");
 
     if (!(Yes_ArmaExperience.checked || No_ArmaExperience.checked)) return false;
     if (!(Yes_MilitaryExperience.checked || No_MilitaryExperience.checked)) return false;
---
+++ javascript/homepage.js
@@ -0,0 +1,38 @@
+const SteamApi = `3E37434837BF21352A799F672E4062F1`;
+
+let UserData = sessionStorage.getItem("userData")
+UserData = JSON.parse(UserData)
+console.log(UserData)
+
+let T_Username = document.getElementById("Username")
+let T_Avatar = document.getElementById("avatar")
+let T_Role = document.getElementById("Role")
+let T_Rank = document.getElementById("RankImg")
+
+T_Username.textContent = "שלום, " + UserData.Name;
+T_Role.textContent = UserData.Position
+T_Rank.src = `img/Rank${13}.png`
+
+if (UserData.Rank > 4) {
+    T_Rank.style.width = "22px"
+    T_Rank.style.transform = `rotate(45deg)`;
+}
+else {
+    T_Rank.style.width = "45px"
+    T_Rank.style.transform = `rotate(-20deg)`;
+}
+
+fetch(`https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${SteamApi}&steamids=${UserData.SteamId}`)
+.then(response => response.text())
+.then(data => {
+     try {
+        const parsedData = JSON.parse(data);
+        const avatarUrl = parsedData.response.players[0].avatarfull;
+        T_Avatar.src = avatarUrl;
+        console.log(parsedData);
+    } catch (error) {
+        console.log("Error")
+        T_Avatar.src = `img/def_img.png`
+    }
+})
+.catch(error => console.error('שגיאה בקשת ה-API:', error));
