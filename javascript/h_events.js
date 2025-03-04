7e0150096c660b40b958884ca53f7112a1dda2dd
OfirS360 <koren222ofir@gmail.com>
2025-03-05 01:27:25 +0200

    Add files via upload

--- javascript/h_events.js
+++ javascript/h_events.js
@@ -1,38 +1,86 @@
-let UserData = sessionStorage.getItem("userData")
-UserData = JSON.parse(UserData)
-let SteamAvatar = sessionStorage.getItem("SteamAvatar")
-console.log(SteamAvatar)
+const today = new Date();
 
-if (!UserData) {
-    alert("נא לבצע התחברות לפני כניסה לאזור האישי!")
-    window.location.replace("index.html");
+let year = today.getFullYear();
+let month = today.getMonth() + 1;
+let day = today.getDate();
+
+let days = generateMonthDays(year, month);
+let startday = new Date(year, month - 1, 1).getDay()
+let lastDay = new Date(year, month -1, days[days.length -1]).getDay()
+console.log(lastDay)
+
+let firstdays = getLastDaysOfMonth(year, month - 1, startday)
+
+const EventBox = document.getElementById("EventsBox")
+const Calender = document.getElementById("calendar")
+
+for (let i = 0; i < firstdays.length; i++) 
+{
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('c_contact')
+    newDiv.classList.add('c_dark')
+    newDiv.textContent = firstdays[i]
+
+    Calender.appendChild(newDiv)
 }
 
-let T_Username = document.getElementById("Username")
-let T_Avatar = document.getElementById("avatar")
-let T_Role = document.getElementById("Role")
-let T_Rank = document.getElementById("RankImg")
-let B_Disconnect = document.getElementById("Disconnect")
-
-T_Username.textContent = "שלום, " + UserData.Name;
-T_Role.textContent = UserData.Position
-T_Rank.src = `img/Rank${UserData.Rank}.png`
-
-if (SteamAvatar)
-    T_Avatar.src = SteamAvatar
-else
-    T_Avatar.src = "img/def_img.png"
-
-if (UserData.Rank > 4) {
-    T_Rank.style.width = "22px"
-    T_Rank.style.transform = `rotate(45deg)`;
-}
-else {
-    T_Rank.style.width = "45px"
-    T_Rank.style.transform = `rotate(-20deg)`;
+for (let i = 0; i < days.length; i++) 
+{
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('c_contact')
+    newDiv.classList.add('c_btn')
+    if (i == 0)
+        newDiv.classList.add('currect_div')
+    newDiv.id = days[i]
+    newDiv.textContent = days[i]
+    
+    Calender.appendChild(newDiv)
 }
 
-B_Disconnect.onclick = () => {
-    sessionStorage.clear()
-    window.location.replace("index.html");
+for (let i = 0; i < 6 - lastDay; i++) 
+{
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('c_contact')
+    newDiv.classList.add('c_dark')
+    newDiv.textContent = days[i]
+        
+    Calender.appendChild(newDiv)
+}
+
+let daysbnt = document.querySelectorAll('#calendar .c_btn')
+
+
+daysbnt.forEach(day => {
+    day.addEventListener('click', function() {
+        let LastDivPressed = document.querySelector('.currect_div')
+        if (LastDivPressed)
+            LastDivPressed.classList.remove('currect_div')
+        LastDivPressed = this
+        LastDivPressed.classList.add('currect_div')
+    })
+})
+
+
+// כל הימים בחודש
+function generateMonthDays(year, month) {
+    const days = [];
+    const daysInMonth = new Date(year, month, 0).getDate();
+
+    for (let day = 1; day <= daysInMonth; day++) {
+        days.push(day);
+    }
+
+    return days;
+}
+
+// הימים הראשונים בלוח
+function getLastDaysOfMonth(year, month, daysCount) {
+    const daysInMonth = new Date(year, month, 0).getDate();
+    const lastDays = [];
+
+    for (let i = daysInMonth - daysCount + 1; i <= daysInMonth; i++) {
+        lastDays.push(i);
+    }
+
+    return lastDays;
 }
