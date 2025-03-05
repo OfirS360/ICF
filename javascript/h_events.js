9be622608c33516b365ab99b6da7e4f0abc9d7ed
OfirS360 <koren222ofir@gmail.com>
2025-03-05 04:40:01 +0200

    Add files via upload

--- javascript/h_events.js
+++ javascript/h_events.js
@@ -92,12 +92,10 @@ function UpdateCalender() {
         })
     })
 
-    let CurrectMonth = {year, month}
-
-    fetch("https://icf-api-ten.vercel.app/GetCurrentMonthEvents")
+    fetch(`https://icf-api-ten.vercel.app/GetCurrentMonthEvents?Year=${year}&Month=${month}`)
     .then(response => response.json())
     .then(data => {
-        if (data.results.length > 0) {
+        if (data.results && data.results.length > 0) {
             console.log(data)
         } 
     })
