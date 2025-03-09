0f4ee32dcbfe952621ee3443f58e162d6f86256d
OfirS360 <koren222ofir@gmail.com>
2025-03-09 17:46:46 +0200

    Add files via upload

--- javascript/h_createevent.js
+++ javascript/h_createevent.js
@@ -1,9 +1,3 @@
-const E_Title = document.getElementById("Title")
-const E_Description = document.getElementById("Description")
-const E_Date = document.getElementById("Date")
-const E_Time = document.getElementById("Time")
-const E_Type = document.getElementById("type")
-
 const SubmitBtn = document.getElementById("submit")
 
 SubmitBtn.onclick = () => {
@@ -20,24 +14,24 @@ SubmitBtn.onclick = () => {
     {
         console.log(FormEventDate)
         alert("בדיקה")
-    }
-    
-    fetch("https://icf-api-ten.vercel.app/EventFormSend", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(FormEventDate)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Success:", data);
-        console.log("Form submitted successfully!");
 
-        window.location.href = "h_events.html";
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        console.log("Error submitting the form.");
-    });
+        fetch("https://icf-api-ten.vercel.app/EventFormSend", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(FormEventDate)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Success:", data);
+            console.log("Form submitted successfully!");
+    
+            window.location.href = "h_events.html";
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            console.log("Error submitting the form.");
+        });
+    }
 }
