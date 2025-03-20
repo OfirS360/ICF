0674d20199ac7124c8efe1ac37b8e3fa91b02ae6
OfirS360 <koren222ofir@gmail.com>
2025-03-20 16:17:19 +0200

    Add files via upload

---
+++ javascript/e_createitem.js
@@ -0,0 +1,47 @@
+let ItemForm = document.getElementById("CreateEventForm")
+
+ItemForm.onsubmit = async (event) => {
+    event.preventDefault();
+
+    let fileInput = document.getElementById("Image");
+    let file = fileInput.files[0];
+
+    let reader = new FileReader();
+
+        reader.onload = function(e) {
+            let base64String = e.target.result;
+
+            let FormItemData = {
+                Title: document.getElementById("Title").value,
+                Description: document.getElementById("Description").value,
+                ItemId: document.getElementById("ItemId").value,
+                Weight: document.getElementById("Weight").value,
+                Space: document.getElementById("Space").value,
+                Limit: document.getElementById("Limit").value,
+                Team: document.getElementById("Team").value,
+                Category: document.getElementById("Category").value,
+                Pakal: document.getElementById("Pakal").value,
+                Type: document.getElementById("Type").value,
+                Image: base64String,
+            };
+
+            fetch("https://icf-api-ten.vercel.app/EventFormSend", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(FormEventData)
+            })
+            .then(response => response.json())
+            .then(data => {
+                alert("החפץ נוסף בהצלחה")
+                location.reload(false)
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                console.log("Error submitting the form.");
+            });
+        };
+
+        reader.readAsDataURL(file);
+}
