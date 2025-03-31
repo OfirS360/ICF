11d25a0cfc704ad229e37a8ed64c5690dc1a3810
OfirS360 <koren222ofir@gmail.com>
2025-04-01 01:27:12 +0300

    Add files via upload

---
+++ javascript/my_team.js
@@ -0,0 +1,17 @@
+let TeamMembers
+
+async function initializePage() {
+    await GetAllTeamMembers(UserData2.Team)
+}
+
+async function GetAllTeamMembers(Team) {
+    const response = await fetch(`https://icf-api-ten.vercel.app/GetAllTeamMembers/${Team}`)
+    let data = await response.json();
+    
+    console.log(data)
+
+    if (data.results) {
+        sessionStorage.setItem("Members", JSON.stringify(data.results))
+        TeamMembers = data.results
+    }
+}
