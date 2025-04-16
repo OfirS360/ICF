e13b541731a446f50e6d9a2cc0ad52c79c5db165
OfirS360 <koren222ofir@gmail.com>
2025-04-16 18:13:22 +0300

    Add files via upload

--- javascript/alert_box.js
+++ javascript/alert_box.js
@@ -12,12 +12,14 @@ function PopAlert(Title, IsGoodNum, Description) {
         IsRunning = true
 
         AlertBox.style.opacity = 1;
+        AlertBox.style.zIndex = 100;
         AlertTitle.innerHTML = `${Title} <img id="AlertImg" src="../img/${Pics[IsGoodNum]}.png">`;
         AlertDescription.textContent = Description
 
         setTimeout(() => {
             AlertBox.style.opacity = 0;
             IsRunning = false
+            AlertBox.style.zIndex = -1;
         }, 3000);
     }
 }
